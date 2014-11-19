2c92bbf7a3bf27556c3162b77cc6e4af0c0c5a17
Mahmud Rasih ÇELENLİOĞLU <sadican@users.noreply.github.com>
2014-11-19 14:24:45 +0200

    Update ubuntu_new.sh
    chrome and sublime text 3 is added

--- ubuntu_new.sh
+++ ubuntu_new.sh
@@ -15,4 +15,16 @@ echo -e "\nUPGRADING PACKAGES\n"
 sudo apt-get upgrade
 
 echo -e "\nINSTALLING NEW PACKAGES\n"
-sudo apt-get install -y git build-essential default-jdk ant python-dev nautilus-open-terminal filezilla unity-tweak-tool gnome-tweak-tool
+sudo apt-get install -y git build-essential dpkg default-jdk ant python-dev nautilus-open-terminal filezilla unity-tweak-tool gnome-tweak-tool
+
+echo -e "\nDOWNLOADING GOOGLE CHROME\n"
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+echo -e "\nINSTALLING GOOGLE CHROME\n"
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+echo -e "\nDOWNLOADING SUBLIME TEXT 3\n"
+wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3065_amd64.deb
+
+echo -e "\nINSTALLING SUBLIME TEXT 3\n"
+sudo dpkg -i sublime-text_build-3065_amd64.deb
