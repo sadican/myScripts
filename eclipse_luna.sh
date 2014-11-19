920d1c4631509bbacbefe4a2dc0b827d5f01b73e
Mahmud Rasih ÇELENLİOĞLU <sadican@users.noreply.github.com>
2014-11-19 13:58:05 +0200

    Create eclipse_luna.sh
    Installation script of Eclipse Luna

---
+++ eclipse_luna.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Fail on error
+set -e
+
+# Fail on unset var usage
+set -o nounset
+
+ECLIPSE="eclipse-standard-luna-SR1-linux-gtk-x86_64"
+ECLIPSELUNA="eclipse_luna"
+ECLIPSELAUNCHERCODE="[Desktop Entry]\nName=Eclipse Luna\nType=Application\nExec=/opt/$ECLIPSELUNA/eclipse\nTerminal=false\nIcon=/opt/$ECLIPSELUNA/icon.xpm\nComment=Integrated Development Environment\nNoDisplay=false\nCategories=Development;IDE;\nName[en]=Eclipse Luna"
+
+echo -e "\nDOWNLOADING ECLIPSE LUNA\n"
+wget http://developer.eclipsesource.com/technology/epp/luna/$ECLIPSE.tar.gz
+
+echo -e "\nEXTRACTING COMPRESSED FILE\n"
+tar -zxvf $ECLIPSE.tar.gz
+
+echo -e "\nRENAMING DIRECTORY\n"
+mv "eclipse" $ECLIPSELUNA
+
+echo -e "\nMOVING ECLIPSE TO OPT DIRECTORY\n"
+sudo mv $ECLIPSELUNA /opt/
+
+echo -e "\nCREATING LAUNCHER\n"
+echo -e $ECLIPSELAUNCHERCODE > $ECLIPSELUNA.desktop
+
+echo -e "\nMOVING LAUNCHER to /usr/share/applications/\n"
+sudo mv $ECLIPSELUNA.desktop /usr/share/applications/
+
+echo -e "\nGIVING EXECUTION PERMISSON TO LAUNCHER\n"
+sudo chmod +x /usr/share/applications/$ECLIPSELUNA.desktop
+
+exit
