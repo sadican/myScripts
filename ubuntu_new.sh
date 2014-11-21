2115fd653e32b02502c918868d51bc03a4b120a7
Mahmud Rasih ÇELENLİOĞLU <sadican@users.noreply.github.com>
2014-11-21 07:53:18 +0200

    Update ubuntu_new.sh
    bash_aliases are added

--- ubuntu_new.sh
+++ ubuntu_new.sh
@@ -16,16 +16,32 @@ echo -e "\nUPGRADING PACKAGES\n"
 sudo apt-get upgrade
 
 echo -e "\nINSTALLING NEW PACKAGES\n"
-sudo apt-get install -y git build-essential dpkg nautilus-open-terminal filezilla unity-tweak-tool gnome-tweak-tool
+sudo apt-get install -y git build-essential gdebi dpkg nautilus-open-terminal filezilla unity-tweak-tool gnome-tweak-tool
+
+echo -e "\nRESET NAUTILUS\n"
+sudo nautilus -q
+
+echo -e "\nSET BASH ALIASES\n"
+echo "alias update='sudo apt-get update'
+alias upgrade='sudo apt-get upgrade'
+alias install='sudo apt-get install'
+alias autoremove='sudo apt-get autoremove'
+alias autoclean='sudo apt-get autoclean'
+alias reboot='sudo reboot'
+alias shutdown='sudo shutdown -h now'
+alias cmdx='sudo chmod +x'" > /home/$USER/.bash_aliases
+source /home/$USER/.bash_aliases
 
 echo -e "\nDOWNLOADING GOOGLE CHROME\n"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
 echo -e "\nINSTALLING GOOGLE CHROME\n"
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+#sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo gdebi google-chrome-stable_current_amd64.deb
 
 echo -e "\nDOWNLOADING SUBLIME TEXT 3\n"
 wget http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3065_amd64.deb
 
 echo -e "\nINSTALLING SUBLIME TEXT 3\n"
-sudo dpkg -i sublime-text_build-3065_amd64.deb
+#sudo dpkg -i sublime-text_build-3065_amd64.deb
+sudo gdebi sublime-text_build-3065_amd64.deb
